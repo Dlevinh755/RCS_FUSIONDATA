5040971953f489f1758dc845c81889a238c9d79d
Levinh <23644831.vinh@student.iuh.edu.vn>
2025-10-24 22:14:30 +0700

    fix bug

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ from train_mlp import trainmlp
 
 def main(args):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    meta = pd.read_csv("/kaggle/input/amazon-product/meta.csv")
-    review = pd.read_csv("/kaggle/input/amazon-product/reviews.csv")
-    df = review.merge(meta, on="asin")
-    df["file_path"] = "/kaggle/input/amazon-product/images/"+ df["asin"] +".jpg"
+
+    if args.data_path:
+        df = pd.read_csv(args.data_path)
+        df["file_path"] = args.data_path + "images/" + df["asin"] + ".jpg"
+    else:
+        df = None
     model, metrics = trainmlp(df, batch_size=args.batch_size, lr=args.lr, epochs=args.epochs, patience=args.patience, heads=args.heads, device=device)
+    model_path = "mlp_camrec_model.pth"
+    torch.save(model.state_dict(), model_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -19,5 +23,6 @@ if __name__ == "__main__":
     parser.add_argument("--epochs", type=int, default=20)
     parser.add_argument("--patience", type=int, default=5)
     parser.add_argument("--heads", type=int, default=4)
+    parser.add_argument("--data_path", type=str, default="/kaggle/input/amazon-product/")
     args = parser.parse_args()
     main(args)
